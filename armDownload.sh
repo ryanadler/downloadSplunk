b23ca1a9498f50ad6a0a1e04265c4f88eb8f4c25
ryanadler <37856307+ryanadler@users.noreply.github.com>
2019-04-23 07:48:48 -0700

    Update armDownload.sh
    updated armDownload for raspi projects

--- armDownload.sh
+++ armDownload.sh
@@ -5,7 +5,7 @@
 #
 
 filename=$(curl 'https://www.splunk.com/page/previous_releases/universalforwarder#armlinux' | grep Linux-arm | perl -lne '/splunkforwarder\-\d\.\d\.\d(\.\d\-|\-)\w+\-Linux\-arm\.tgz/ && print $&' | head -n 5 | sort -gr | head -n 1)
-version=$(echo $filename | sed 's/splunk-//g' | sed 's/-.*//g')
+version=$(echo $filename | sed 's/splunkforwarder-//g' | sed 's/-.*//g')
 
 
 echo " "
